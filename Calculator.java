23571c6f6f892e9b182f0d8a593ed384005fb3c0
Crabbypattyrie <patrickpardillo14@gmail.com>
2025-07-17 19:55:04 +0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -19,14 +19,19 @@ public class Calculator {
 		given2 = scan.nextInt();
 		
 		System.out.println("1.Addition 2.Subtraction 3.Multiplication 4.Division");
-		System.out.print("Enter the Operation: ");
+		System.out.print("Enter the Mathematical Operation: ");
 		ope = scan.nextInt();
 		
-		if(ope == 1)System.out.println(given1 + given2);
-		else if (ope == 2)System.out.println(given1 - given2);
-		else if (ope == 3)System.out.println(given1 * given2);
-		else if (ope == 4)System.out.println(given1 / given2);
-		else System.out.println("Valid Operation");
+		if(ope == 1)
+			System.out.println("Answer: " + (given1 + given2));
+		else if (ope == 2)
+			System.out.println("Answer: " + (given1 - given2));
+		else if (ope == 3)
+			System.out.println("Answer: " + (given1 * given2));
+		else if (ope == 4)
+			System.out.println("Answer: " + (given1 / given2));
+		else 
+			System.out.println("Valid Operation");
 		
 		
 		
