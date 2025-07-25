a71fb7c44d988494a69e115b33bad5e59d8c8b70
Crabbypattyrie <patrickpardillo14@gmail.com>
2025-07-25 22:52:36 +0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -6,41 +6,45 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		
-		int given1;
-		int given2;
-		int ope;
+		float ge4;
+		float ge3;
+		float ge2;
+		float ge1;
+		String name;
 		
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.print("Entered First Number:");
-		given1 = scan.nextInt();
+		System.out.print("Enter your Name: ");
+		name = scan.nextLine();
 		
-		System.out.print("Entered Second Number:");
-		given2 = scan.nextInt();
+		System.out.print("Enter your grade in Computer programming: ");
+		ge4 = scan.nextFloat();
 		
-		System.out.println("1.Addition 2.Subtraction 3.Multiplication 4.Division");
-		System.out.print("Enter the Mathematical Operation: ");
-		ope = scan.nextInt();
+		System.out.print("Enter your grade in Introducing to Computing: ");
+		ge3 = scan.nextFloat();
 		
-		if(ope == 1)
-			System.out.println("Answer: " + (given1 + given2));
-		else if (ope == 2)
-			System.out.println("Answer: " + (given1 - given2));
-		else if (ope == 3)
-			System.out.println("Answer: " + (given1 * given2));
-		else if (ope == 4)
-			System.out.println("Answer: " + (given1 / given2));
-		else 
-			System.out.println("Valid Operation");
+		System.out.print("Enter your grade in Pathfit: ");
+		ge2 = scan.nextFloat();
+		
+		System.out.print("Enter your grade in Understanding Your Self: ");
+		ge1 = scan.nextFloat();
+		
+		System.out.println("Your Grade in Computer programming     : " + ge4);
+		System.out.println("Your Grade in Introducing to Computing : " + ge3);
+		System.out.println("Your Grade in Pathfit                  : " + ge2);
+		System.out.println("Your Grade in Understanding Your Self  : " + ge1);
+		
+		float ave = (ge1 + ge2 + ge3 + ge4) /4;
+		
+		System.out.println("Your Average is: " + ave);
+		
+		if (ave >= 98)System.out.println("Youre With Highest Honor Congrats" + name);
+		else if (ave >= 95)System.out.println("Youre With High Honor Congrats" + name);
+		else if (ave >= 90)System.out.println("Youre with Honor Congrats" + name);
+		else if (ave >= 75)System.out.println("Youre Passed Congrats" + name);
+		else System.out.println("Youre Failed, Try Again");
 		
 		
-		
-		
-		
-		
-		
-		
-
 	}
 
 }
